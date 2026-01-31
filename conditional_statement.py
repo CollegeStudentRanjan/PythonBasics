16a488e081c7aabfb7238f4ae6d9f3a71b875d00
CollegeStudentRanjan <ranjanranjan3697@gmail.com>
2026-01-31 20:30:30 +0530

    Add files via upload
    This is my begener Code.

---
+++ conditional_statement.py
@@ -0,0 +1,16 @@
+a = 85
+if (a >= 90):
+    print("your grade is A")
+elif(a >= 80):
+    print("Your grade is B")
+
+
+age = 18
+if (age >= 18):
+    print("you are eligible to vote")
+else:
+    print("you are not eligble to vote")
+
+print("your code is end")
+
+
